6e8035b4ec1277364e918210ef4791ad93c50b5c
anonymous0374 <377738226@qq.com>
2018-08-20 08:49:29 +0800

    start to work on cookie and session

--- src/start.ams.js
+++ src/start.ams.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
 import {
   mongoose, stringifyObjId, PORT, DB_NAME, cnn_url, options,
 } from './config';
@@ -8,20 +9,43 @@ import { BasicInfo, User, Asset } from './schemas';
 const EXPRESS_PORT = 3001;
 
 // connect to mongodb server
-mongoose.connect(cnn_url, options,
+mongoose.connect(
+  cnn_url,
+  options,
   () => {
     console.log(`connection to ${DB_NAME} established`);
     const app = express();
+    /*
+    app.use(
+      cookieSession({
+        name: 'session',
+        keys: ['name'],
+        httpOnly: true,
+      }),
+    );
+    */
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
     app.post('/ams/login', (req, res) => {
-      const { params: { name, password } } = req.body;
+      const {
+        params: { name, password },
+      } = req.body;
       User.findOne({ name }).exec((err, user) => {
         res.setHeader('Content-Type', 'application/json');
         if (err || !user) {
-          return res.end(JSON.stringify({ msg: user ? `authentication failed: ${err.toString()}` : 'authentication failed: no such user', code: -1 }));
+          return res.end(
+            JSON.stringify({
+              msg: user
+                ? `authentication failed: ${err.toString()}`
+                : 'authentication failed: no such user',
+              code: -1,
+            }),
+          );
         }
         if (password === user.password) {
+          // req.session.name = user.name;
+          console.info(res);
+          res.setHeader('Set-Cookie', [`name=${user.name}`]);
           return res.end(JSON.stringify({ user, msg: 'successful', code: 0 }));
         }
 
@@ -31,7 +55,9 @@ mongoose.connect(cnn_url, options,
 
     app.post('/ams/user/add', (req, res) => {
       res.setHeader('Content-Type', 'application/json');
-      const { params: { basicInfo, extraInfo } } = req.body;
+      const {
+        params: { basicInfo, extraInfo },
+      } = req.body;
       const newBasicInfo = new BasicInfo({
         city: extraInfo.city,
         gender: extraInfo.gender,
@@ -65,11 +91,26 @@ mongoose.connect(cnn_url, options,
       const { name } = req.query;
       const query = Asset.find({ owner: name });
       const promise = query.exec();
-      promise.then(data => res.end(JSON.stringify({ code: 0, data })),
-        err => res.end(JSON.stringify({ code: -1, msg: `something went wrong: ${err}` })));
+      promise.then(
+        data => res.end(JSON.stringify({ code: 0, data })),
+        err => res.end(JSON.stringify({ code: -1, msg: `something went wrong: ${err}` })),
+      );
     });
+
+    app.get('/ams/logout', (req, res) => {
+      res.setHeader('Content-Type', 'application/json');
+      const { name } = req.query;
+      // need to do some session / cookie management here
+      res.end(
+        JSON.stringify({ code: 0, msg: 'sorry, session / cookie management not in place yet' }),
+      );
+    });
+
     app.listen(EXPRESS_PORT, (data) => {
       console.info(`express server started at: ${EXPRESS_PORT}`);
     });
   },
-  (err) => { console.error(`connection to ${DB_NAME} has failed. err: `, err); });
+  (err) => {
+    console.error(`connection to ${DB_NAME} has failed. err: `, err);
+  },
+);
